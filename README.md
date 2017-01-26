662383415680f4d05ab1fa89e4d8bec58bd13663
Geoff Hackett <ghackett@gmail.com>
2017-01-26 00:56:52 -0500

    import from private repo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 jenkins-pipelines
 =================
 
+Helper repo for jenkins pipelines
+Load these with the Pipeline Remote Loader Plugin (https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Remote+Loader+Plugin)
+See individual files for other required plugins
+
 ### License
 MIT: https://github.com/episode6/jenkins-pipelines/blob/master/LICENSE
---
+++ gradle/GradleRunner.groovy
@@ -0,0 +1,62 @@
+/**
+ * Builds, tests and collects reports on a gradle project
+ *
+ * Required plugins...
+ * Pipeline Utility Steps Plugin: https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Utility+Steps+Plugin
+ * HTML Publisher Plugin: https://wiki.jenkins-ci.org/display/JENKINS/HTML+Publisher+Plugin
+ */
+def version = '0.0.1-SNAPSHOT'
+
+def buildAndTest() {
+  stage('build') {
+    sh "./gradlew clean assemble"
+  }
+
+  stage('test') {
+    runTests()
+  }
+}
+
+def runTests() {
+  Exception err
+  try {
+    sh "./gradlew check"
+  } catch (Exception e) {
+    err = e
+    currentBuild.result = "FAILURE"
+  } finally {
+    collectReports()
+    if (err) {
+      throw err
+    }
+  }
+}
+
+def collectReports() {
+  junit(
+      keepLongStdio: true,
+      testResults: '**/build/test-results/**/*.xml')
+  collectHtmlReports(glob: "**/build/reports/**/index.html")
+}
+
+def collectHtmlReports(Map findIndexFilesParams) {
+  def htmlIndexFiles = findFiles(findIndexFilesParams)
+  for (int i = 0; i < htmlIndexFiles.length; i++) {
+    def it = htmlIndexFiles[i]
+    def path = it.path.substring(0, it.path.length() - "/index.html".length())
+    def name = path.substring(0, path.indexOf("/"))
+    def prefix = path.substring(path.lastIndexOf("/") + 1, path.length())
+    if (name != "build") {
+      prefix = "${name} ${prefix}"
+    }
+    publishHTML([
+        allowMissing: false,
+        alwaysLinkToLastBuild: true,
+        keepAll: false,
+        reportDir: path,
+        reportFiles: 'index.html',
+        reportName: "${prefix} report (${i})"])
+  }
+}
+
+return this
