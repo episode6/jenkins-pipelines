58a18e3511dac4fc6f720e5be0cd48ea29c939e8
Geoff Hackett <ghackett@gmail.com>
2017-05-05 16:36:01 -0400

    trying one more fix before reverting

--- gradle/GradleRunner.groovy
+++ gradle/GradleRunner.groovy
@@ -106,8 +106,8 @@ def outputLogFilename(String stageName) {
 }
 
 def getProjectVersion() {
-  sh "./gradlew properties | grep -o '^version: .*$' | sed 's/version: //' > __gradle_project.version"
-  return readFile("__gradle_project.version")
+  sh './gradlew properties | grep -o \'^version: .*$\' | sed \'s/version: //\' > __gradle_project.version'
+  return "${readFile("__gradle_project.version")}"
 }
 
 return this
