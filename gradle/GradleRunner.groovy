d7d9461ff84625bf8b999a3f7686f39f11e91ca1
Geoff Hackett <ghackett@gmail.com>
2017-05-08 09:40:37 -0400

    gradle spits projectVersion out as unspecified

--- gradle/GradleRunner.groovy
+++ gradle/GradleRunner.groovy
@@ -20,7 +20,7 @@ def buildAndTest() {
 def maybeDeploy() {
   stage('deploy') {
     def projectVersion = getProjectVersion()
-    if (!projectVersion) {
+    if (!projectVersion || projectVersion == "unspecified") {
       def err = "Could not read projectVersion for job: ${env.JOB_NAME}, deploy failed."
       notifyPushbullet(err)
       error(err)
